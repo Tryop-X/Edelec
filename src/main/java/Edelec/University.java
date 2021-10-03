ec4a82b57b55893163f6da5603fcc380ffc1ed3a
Pixgel <pixgel13040@gmail.com>
2021-10-03 00:21:30 -0500

    inicio del proyecto

---
+++ src/main/java/Edelec/University.java
@@ -0,0 +1,4 @@
+package Edelec;
+
+public class University {
+}
---
+++ src/test/java/com/example/edelec/EdelecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edelec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdelecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
