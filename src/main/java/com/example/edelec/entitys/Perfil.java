21993645fa8d64df202d47510510938d496a6c4d
Diego Renato Torres Añorga <diegot14321@gmail.com>
2021-11-04 22:55:54 -0500

    Update Perfil.java

--- src/main/java/com/example/edelec/entitys/Perfil.java
+++ src/main/java/com/example/edelec/entitys/Perfil.java
@@ -2,8 +2,12 @@ package com.example.edelec.entitys;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "perfiles")
@@ -12,6 +16,28 @@ import javax.persistence.*;
 public class Perfil {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idPerfil;
+    private Long perfilId;
 
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm")
+    @Column(name = "Fecha", nullable = false)
+    private LocalDateTime fecha;
+
+    @NotNull
+    @Size(min = 20)
+    @Column(name = "descripción", nullable = true, length = 20, unique = true)
+    private String descripcion;
+
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
