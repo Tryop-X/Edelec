2c4968e80c265b59395fce4a5f92e6d813ab4f4c
Manuel Antonio Flórez Diaz <90992592+Lext69@users.noreply.github.com>
2021-12-06 22:56:02 -0500

    CRUD Sede

--- src/main/java/com/example/edelec/controllers/SedeController.java
+++ src/main/java/com/example/edelec/controllers/SedeController.java
@@ -14,3 +14,38 @@ public class SedeController {
         this.sedeService = sedeService;
     }
 }
+@PostMapping
+    public ResponseEntity<Sede> createCarrera(@Valid @RequestBody Sede sede) {
+        Sede sedeNew =sedeService.createSede(sede);
+        return  new ResponseEntity<Sede>(sedeNew, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Sede>> GetAllSedes() {
+        List<Sede> sedesNew =sedeService.getAllSedes();
+        return  new ResponseEntity<List<Sede>>(sedesNew, HttpStatus.OK);
+    }
+
+    @GetMapping("/name/{nombreSede}")
+    public ResponseEntity<List<Sede>> GetSedesNombre(@PathVariable("nombreSede") String nombreSede) {
+        List<Sede> sedesNew =sedeService.getSedesNombre(nombreSede);
+        return  new ResponseEntity<List<Sede>>(sedesNew, HttpStatus.OK);
+    }
+
+    @GetMapping("/{ubicacionSede}")
+    public ResponseEntity<List<Sede>> GetSedesUbicacion(@PathVariable("ubicacionSede") String ubicacionSede) {
+        List<Sede> sedesNew =sedeService.getSedesUbicacion(ubicacionSede);
+        return  new ResponseEntity<List<Sede>>(sedesNew, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Sede> updateSede(@Valid @RequestBody Sede sede){
+        Sede sedeUpdate =sedeService.createSede(sede);
+        return  new ResponseEntity<Sede>(sedeUpdate, HttpStatus.OK);
+    }
+
+    @PutMapping("/{idSede}")
+    public ResponseEntity<Void> deleteSede(@PathVariable("idSede") Integer idSede){
+        sedeService.deleteSede(idSede);
+        return  new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
