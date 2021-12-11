eee6b65bbeebc213440e6a41bca6ef0c40d0aaf5
serjaumen22 <78753750+serjaumen22@users.noreply.github.com>
2021-12-11 15:24:58 -0500

    Update CommentServiceImpl.java

--- src/main/java/com/example/edelec/services/impl/CommentServiceImpl.java
+++ src/main/java/com/example/edelec/services/impl/CommentServiceImpl.java
@@ -1,23 +1,40 @@
 package com.example.edelec.services.impl;
 
 import com.example.edelec.entitys.Comments;
+import com.example.edelec.entitys.Universidad;
+import com.example.edelec.entitys.Usuario;
+import com.example.edelec.exception.ResourceNotFoundException;
 import com.example.edelec.repositories.CommentRepository;
+import com.example.edelec.repositories.UniversidadRepository;
+import com.example.edelec.repositories.UsuarioRepository;
 import com.example.edelec.services.CommentService;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class CommentServiceImpl implements CommentService {
 private final CommentRepository commentRepository;
+private final UsuarioRepository usuarioRepository;
+private final UniversidadRepository universidadRepository;
 
-    public CommentServiceImpl(CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentRepository commentRepository, UsuarioRepository usuarioRepository, UniversidadRepository universidadRepository) {
         this.commentRepository = commentRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.universidadRepository = universidadRepository;
     }
 
     @Override
     public Comments createComment(Comments comment) {
+        Usuario usuario=usuarioRepository.findById(comment.getUser().getIdUsuario())
+                .orElseThrow(()-> new ResourceNotFoundException("No existe el Usuario"));
+        Universidad universidad =universidadRepository.findById(comment.getUniversidad().getIdUniversidad())
+                .orElseThrow(()-> new ResourceNotFoundException("No existe la universidad"));
+        comment.setTiempo(LocalDateTime.now());
+        comment.setUser(usuario);
+        comment.setUniversidad(universidad);
         return commentRepository.save(comment);
     }
 
@@ -38,5 +55,12 @@ private final CommentRepository commentRepository;
     public List<Comments> listComments() {
         return commentRepository.findAll();
     }
+
+    @Override
+    public List<Comments> getCommentsByUniversidadId(Integer Id) {
+        List<Comments> comments=commentRepository.findByUniversidadIdUniversidad(Id);
+        return comments;
+    }
 }
+
 
