ee5784cd3ec927483cc962acfb58190d0ce8a00f
serjaumen22 <78753750+serjaumen22@users.noreply.github.com>
2021-12-11 06:51:38 -0500

    Update CommentRepository.java

--- src/main/java/com/example/edelec/repositories/CommentRepository.java
+++ src/main/java/com/example/edelec/repositories/CommentRepository.java
@@ -1,1 +1,15 @@
+package com.example.edelec.repositories;
+
+import com.example.edelec.entitys.Comments;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comments,Long> {
+
+
+
+
+
+}
 
