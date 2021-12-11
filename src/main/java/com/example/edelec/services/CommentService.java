6f3c07d2342c3a9e9216a6646122fedb341b31ec
serjaumen22 <78753750+serjaumen22@users.noreply.github.com>
2021-12-11 05:30:11 -0500

    Update CommentService.java

--- src/main/java/com/example/edelec/services/CommentService.java
+++ src/main/java/com/example/edelec/services/CommentService.java
@@ -1,1 +1,15 @@
+package com.example.edelec.services;
+
+
+import com.example.edelec.entities.Comments;
+
+import java.util.List;
+
+public interface CommentService {
+
+    Comments createComment(Comments comment);
+    Comments updateComment(Comments comment);
+    List<Comments> listComments();
+
+}
 
