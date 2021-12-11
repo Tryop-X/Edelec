6407d33a1ca2e0bed4a0338dcf21d46b8dfef481
serjaumen22 <78753750+serjaumen22@users.noreply.github.com>
2021-12-11 15:30:29 -0500

    Update Universidad.java

--- src/main/java/com/example/edelec/entitys/Universidad.java
+++ src/main/java/com/example/edelec/entitys/Universidad.java
@@ -1,6 +1,6 @@
 package com.example.edelec.entitys;
 
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
