371a8fc325bc5238f478dbe94b4a2e1150925635
serjaumen22 <78753750+serjaumen22@users.noreply.github.com>
2021-12-11 05:26:35 -0500

    Create CommentController.java

---
+++ src/main/java/com/example/edelec/controllers/CommentController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Comments;
+import com.example.demo.services.CommentService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Comments> createComment(@RequestBody Comments comment){
+        Comments commentNew =commentService.createComment(comment);
+        return new ResponseEntity<>(commentNew, HttpStatus.CREATED);
+    }
+    @PutMapping
+    public ResponseEntity<Comments> updateComment(@RequestBody Comments comment){
+        Comments commentUpdate=commentService.updateComment(comment);
+        return new ResponseEntity<>(commentUpdate, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<Comments>>  getAllComments(){
+        List<Comments> comments= commentService.listComments();
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+}
